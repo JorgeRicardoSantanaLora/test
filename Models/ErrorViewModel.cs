92b9a835ca591c9781729a4aa41ade054bbdf463
JorgeRicardoSantanaLora <jorge.santana@gorico.com.do>
2023-01-18 11:14:55 -0400

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GORICO_TEST.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
